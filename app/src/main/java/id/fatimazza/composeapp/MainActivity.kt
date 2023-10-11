95358cf5ea0feccc5331801303082c0c15e08640
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-11 15:45:40 +0700

    Basic UI - Add Surface to add background with Material Theme

--- app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
+++ app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,9 +24,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column {
-        Text(text = "Hello, ")
-        Text(text = name)
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column {
+            Text(text = "Hello, ")
+            Text(text = name)
+        }
     }
 }
 
