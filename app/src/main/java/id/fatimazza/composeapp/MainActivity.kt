d7df52c739951c03fa37d0e16bec970b38a1626f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-12 06:27:47 +0700

    Basic UI - Add Elevated Button

--- app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
+++ app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,6 +29,9 @@ fun Greeting(name: String) {
         Column {
             Text(text = "Hello, ")
             Text(text = name)
+        }
+        ElevatedButton(onClick = {  }) {
+            Text(text = "Show More")
         }
     }
 }
