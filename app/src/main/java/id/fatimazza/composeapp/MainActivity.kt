5a689399be6058dd33aff36d85743abcffc53442
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-12 12:54:26 +0700

    Material Design - Add Preview for Dark Mode

--- app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
+++ app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package id.fatimazza.composeapp
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -118,6 +119,11 @@ fun Greeting(name: String) {
 }
 
 @Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun GreetingPreview() {
     ComposeAppTheme {
