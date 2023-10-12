9df9b153c78544f8182763aa070703d63076fd38
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-12 07:14:10 +0700

    Scrollable List - Add Box Layout to display empty Text

--- app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
+++ app/src/main/java/id/fatimazza/composeapp/MainActivity.kt
@@ -3,6 +3,7 @@ package id.fatimazza.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,8 +52,13 @@ fun MyList(
         color = MaterialTheme.colorScheme.background,
         modifier = Modifier.fillMaxSize()
     ) {
-        if(names.isEmpty()) {
-            Text(text = "No one here :(")
+        if (names.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "No one here :(")
+            }
         } else {
             LazyColumn {
                 items(items = names) {
